f9e9acf3d92a0791c36298521b05f55a725e189a
Viktor Yordanov <87126339+mahlichavpm@users.noreply.github.com>
2022-10-13 15:44:28 +0300

    added components folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import BlueBtn from './reusibleComponents/BlueBtn';
 
 function App() {
   return (
     <>
-    
+    <BlueBtn/>
     </>
   )
 }
---
+++ src/reusibleComponents/BlueBtn.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import './BlueBtn.css';
+
+
+function BlueBtn(props) {
+  return (
+    <Button className='blue-btn'></Button>
+  )
+}
+
+export default BlueBtn
